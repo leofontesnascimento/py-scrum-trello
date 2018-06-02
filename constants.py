3eb092db65b47e9d0984c9c94c454783d001ba88
Leonardo Nascimento <lnascimento.mail@gmail.com>
2018-06-02 11:42:28 -0400

    Adds class with all project constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class Constants:
+    app_name = 'PyScrumTrello'
+    app_key = 'YOUR_APP_KEY'
+    app_secret = 'YOUR_APP_SECRET'
+    request_token_url = 'https://trello.com/1/OAuthGetRequestToken'
+    authorize_token_url = 'https://trello.com/1/OAuthAuthorizeToken'
+    access_token_url = 'https://trello.com/1/OAuthGetAccessToken'
+    base_url = 'https://trello.com/1/authorize/',
+    api_url = 'https://api.trello.com',
+    boards_route_url = '/1/members/me/boards',
+    cards_route_url = '/1/boards/{}/cards',
+    lists_route_url = '/1/boards/{}/lists',
+    boards_file_name = 'boards.json',
+    cards_file_name = 'cards.json',
+    lists_file_name = 'lists.json'
